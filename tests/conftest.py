ffaf51004186fd4f13c3c264f3841dd720a906f8
Alan Bandeira <alan.p.bandeira@gmail.com>
2019-11-25 15:24:17 -0300

    small fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,19 @@
 import pytest
 import random
 
-from src.shippingservice.domain.entity.zipcode_fee_entity import (
-    ZipcodeFeeEntity
+from src.app.domain.entity.region_fee_entity import (
+    RegionFeeEntity
 )
 
-from src.shippingservice.domain.entity.region_entity import RegionEntity
+from src.app.domain.entity.region_entity import RegionEntity
 
 @pytest.fixture(scope='module')
 def new_zipcode_fee():
-    new_region = RegionEntity("Some State", 60417235, 60455996)
+    new_region = RegionEntity(
+        "Some State", 60417235, 60455996,
+        random.choice([5,10,15,20,25]), random.random())
 
-    new_fee = ZipcodeFeeEntity(
+    new_fee = RegionFeeEntity(
         random.randint(10, 30), random.choice([5,10,15,20,25]), new_region)
 
     return new_fee
