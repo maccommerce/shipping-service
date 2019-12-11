9641533bdb0867b657a8c8afabf417b43b8d945b
Alan Bandeira <alan.p.bandeira@gmail.com>
2019-12-11 15:13:11 -0300

    configuraitions for production enviroment

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,21 +4,50 @@ from flask import Flask
 from flask_restful import Api
 from flask_mongoengine import MongoEngine
 from flask_jwt_extended import JWTManager
-from .config.app_config import ServiceConfig, DevConfig
+from .config.app_config import env_config
 from .config import bootstrapDB
-from .web.resources.shipping_fee import ShippinCost
+from .web.resources.shipping_fee import ShippingCost
+from .web.router.blueprints import api_blueprint
 
 
-bootstrapDB.run()
-
-app = Flask(__name__)
-app.config.from_object(DevConfig)
-api = Api(app)
-db = MongoEngine(app)
-jwt = JWTManager(app)
-
-api.add_resource(ShippinCost, '/cost')
+api = Api(api_blueprint)
+db = MongoEngine()
+jwt = JWTManager()
 
 
+def create_prod_app(config_name: str) -> Flask:
+    """
+    Build Flask application and setup development configurations
+    """
+    bootstrapDB.run()
+    app = Flask(__name__)
+    app.config.from_object(env_config[config_name])
+    env_config[config_name].init_app(app)
 
-# from app import resources, web
+    api.init_app(app)
+    db.init_app(app)
+    api.add_resource(ShippingCost, '/cost')
+
+    app.register_blueprint(api_blueprint)
+
+    return app
+
+
+def create_dev_app(config: str) -> Flask:
+    """
+    Build Flask application and setup production configurations 
+    """
+    
+    pass
+
+
+
+# app = Flask(__name__)
+# app.config.from_object(DevConfig)
+# api = Api(app)
+# db = MongoEngine(app)
+# jwt = JWTManager(app)
+
+
+
+
